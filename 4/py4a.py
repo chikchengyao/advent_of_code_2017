b6e48ef514e9a485db1d46519fac70d6491d3abb
ChengYao Chik <chikben@gmail.com>
2017-12-06 22:41:36 +0800

    Complete 4

---
+++ 4/py4a.py
@@ -0,0 +1,18 @@
+def isValid(xs):
+	xs.sort()
+	for i in range(len(xs) - 1):
+		if xs[i] == xs[i+1]:
+			return False
+	return True
+
+# Check for duplicates
+n = 0
+f = open("input.txt")
+for line in f:
+	if isValid(line.split()):
+		n+=1
+
+print(n)
+
+# Check for duplicates
+
---
+++ 4/py4b.py
@@ -0,0 +1,18 @@
+def isValid(xs):
+	for i in range(len(xs)):
+		xs[i] = sorted(list((xs[i])))
+	xs.sort()
+	for i in range(len(xs) - 1):
+		if xs[i] == xs[i+1]:
+			return False
+	return True
+
+# Check for duplicates
+n = 0
+f = open("input.txt")
+for line in f:
+	if isValid(line.split()):
+		n+=1
+
+print(n)
+
