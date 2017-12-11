f8ef21619d40225d84ff9f8381ea151da054cc02
ChengYao Chik <chikben@gmail.com>
2017-12-11 15:41:36 +0800

    Fix critical bug in 11 - original solution not general

--- 11/11.py
+++ 11/11.py
@@ -7,6 +7,14 @@ y = 0
 
 maxdist = 0
 
+def get_dist(x,y):
+    if x*y >= 0:
+        x, y = abs(x), abs(y)
+        return max(x,y)
+    else:
+        x, y = abs(x), abs(y)
+        return x + y
+
 for step in inp:
     if step == "n":
         y += 1
@@ -24,10 +32,11 @@ for step in inp:
         y -= 1
     else:
         print("ERRORRRR")
-    maxdist = max(x,y) if max(x,y) > maxdist else maxdist
+    if get_dist(x,y) > maxdist:
+        maxdist = get_dist(x,y)
 
 # Current distance
-print(max(x,y))
+print(get_dist(x,y))
 
 # Max distance
 print(maxdist)
